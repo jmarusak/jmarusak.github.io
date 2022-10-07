3ca827073e9e2d5635fb51ca5505a8cc61052020
jmarusak <jmarusak@users.noreply.github.com>
2022-10-07 15:50:23 -0400

    swift URL session

--- front-end/Sources/client/main.swift
+++ front-end/Sources/client/main.swift
@@ -1,1 +1,25 @@
-print("Hello, world!")
+import Foundation
+
+func startLoad() {
+    let url = URL(string: "https://www.example.com/")!
+    let task = URLSession.shared.dataTask(with: url) { data, response, error in
+        if let error = error {
+            print("\(error)")
+            return
+        }
+        guard let httpResponse = response as? HTTPURLResponse,
+            (200...299).contains(httpResponse.statusCode) else {
+            print("\(response!)")
+            return
+        }
+        if let mimeType = httpResponse.mimeType, mimeType == "text/html",
+            let data = data,
+            let string = String(data: data, encoding: .utf8) {
+
+            print(string)
+        }
+    }
+    task.resume()
+}
+
+startLoad()
